d66b79cbc934b9e983c223aa26d4fd3bad619ee7
mor-lamntt <lamntt@mor.com.vn>
2024-10-14 13:06:29 +0700

    feat: update for IOS

--- src/components/ImageZoom.tsx
+++ src/components/ImageZoom.tsx
@@ -64,28 +64,23 @@ const Zoomable: ForwardRefRenderFunction<ImageZoomRef, ImageZoomProps> = (
   const translateYSharedValue = useSharedValue(0);
   const scaleSharedValue = useSharedValue(1);
 
-  const [imageWidth, setImageWidth] = useState(0);
+  const [imageWidth, setImageWidth] = useState(1);
   const [imageHeight, setImageHeight] = useState(1);
 
-  const [containerWidth, setContainerWidth] = useState(0);
+  const [containerWidth, setContainerWidth] = useState(1);
   const [containerHeight, setContainerHeight] = useState(1);
 
+  const [isLoaded, setIsLoaded] = useState(false);
+
   const aspectRatio = useMemo(() => {
-    return imageWidth / imageHeight;
+    return imageWidth / imageHeight ;
   }, [imageHeight, imageWidth]);
+
+  // const aspectRatio = useSharedValue(1);
 
   const onPinch = useCallback(
     (e) => {
       if (pins) {
-        // pinsRef.current?.forEach((pin: any, index) => {
-        //   if ((maxScale && e.scale * scaleRef.current < maxScale)){
-        //     pin.setNativeProps({
-        //       style: {
-        // transform: [{ scale: 1/  (e.scale * scaleRef.current)}, { translateX: -11* e.scale * scaleRef.current }, { translateY: -11* e.scale * scaleRef.current }],
-        //       },
-        //     });
-        //   }
-        // });
         scaleSharedValue.value = clamp(
           e.scale * scaleRef.current,
           minScale ?? 1,
@@ -184,20 +179,20 @@ const Zoomable: ForwardRefRenderFunction<ImageZoomRef, ImageZoomProps> = (
 
   // translateYSharedValue.value = translateY > 50 ? translateY : 0;
 
-  useEffect(() => {
-    translateYSharedValue.value = translateY > 50 ? translateY : 0;
-  }, [translateY]);
+  // useEffect(() => {
+  //   translateYSharedValue.value = translateY > 50 ? translateY : 0;
+  // }, [translateY]);
 
-  const animatedStylesContainer = useAnimatedStyle(() => ({
-    transform: [
-      {
-        translateY: withTiming(
-          ((-translateYSharedValue.value / 100) * imageWidth * containerWidth) /
-            containerHeight
-        ),
-      },
-    ],
-  }));
+  // const animatedStylesContainer = useAnimatedStyle(() => ({
+  //   transform: [
+  //     {
+  //       translateY: withTiming(
+  //         ((-translateYSharedValue.value / 100) * imageWidth * containerWidth) /
+  //           containerHeight
+  //       ),
+  //     },
+  //   ],
+  // }));
 
   const animatedStyleScale = useAnimatedStyle(() => ({
     transform: [
@@ -208,75 +203,87 @@ const Zoomable: ForwardRefRenderFunction<ImageZoomRef, ImageZoomProps> = (
   }));
 
   return (
-    <Animated.View
-      style={[{ flex: 1, justifyContent: 'center' }, animatedStylesContainer]}
+    <View
+      style={[{ flex: 1, justifyContent: 'center' }]}
     >
-      <GestureDetector gesture={gestures}>
-        <Animated.View
-          ref={imageRef as React.RefObject<any>}
-          style={[
-            { position: 'relative', backgroundColor: 'orange' },
-            animatedStyle,
-          ]}
-        >
-          <Animated.Image
-            style={[styles.image, style, { aspectRatio }]}
-            source={{ uri }}
-            resizeMode="contain"
-            onLayout={(event) => {
-              onZoomableLayout(event);
-              setContainerWidth(event.nativeEvent.layout.width);
-              setContainerHeight(event.nativeEvent.layout.height);
-            }}
-            src={src}
-            {...props}
-          />
-          {pins &&
-            pins.map((booth: any, index: number) => {
-              return (
-                <Animated.View
-                  ref={(el) => {
-                    if (!pinsRef.current) {
-                      pinsRef.current = [];
-                    }
-                    if (el) {
-                      pinsRef.current[index] = el as any;
-                    }
+
+
+    {
+              isLoaded ?       <GestureDetector gesture={gestures}>
+              <Animated.View
+                ref={imageRef as React.RefObject<any>}
+                style={[
+                  { position: 'relative', backgroundColor: 'orange' },
+                  animatedStyle,
+                ]}
+              >
+                <Animated.Image
+                  key={`${imageHeight}-${imageWidth}-${aspectRatio}`}
+                  style={[styles.image, style, { aspectRatio }]}
+                  source={{ uri }}
+                  resizeMode="contain"
+                  onLayout={(event) => {
+                    onZoomableLayout(event);
+                    setContainerWidth(event.nativeEvent.layout.width);
+                    setContainerHeight(event.nativeEvent.layout.height);
                   }}
-                  key={index}
-                  style={[
-                    {
-                      position: 'absolute',
-                      zIndex: 100,
-                      top: `${booth.y}%`,
-                      left: `${booth.x}%`,
-                      width: 22,
-                      height: 22,
-                      alignItems: 'center',
-                      justifyContent: 'center',
-                    },
-                    animatedStyleScale,
-                  ]}
-                >
-                  {booth.render()}
-                </Animated.View>
-              );
-            })}
-        </Animated.View>
-      </GestureDetector>
-      <Image
-        source={{ uri: src }}
-        style={{ display: 'none' }}
-        onLoad={({
-          nativeEvent: {
-            source: { width, height },
-          },
-        }) => {
-          setImageWidth(width);
-          setImageHeight(height);
-        }}
-      />
-    </Animated.View>
+                  src={src}
+                  {...props}
+                />
+                {pins &&
+                  pins.map((booth: any, index: number) => {
+                    return (
+                      <Animated.View
+                        ref={(el) => {
+                          if (!pinsRef.current) {
+                            pinsRef.current = [];
+                          }
+                          if (el) {
+                            pinsRef.current[index] = el as any;
+                          }
+                        }}
+                        key={index}
+                        style={[
+                          {
+                            position: 'absolute',
+                            zIndex: 100,
+                            top: `${booth.y}%`,
+                            left: `${booth.x}%`,
+                            width: 22,
+                            height: 22,
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                          },
+                          animatedStyleScale,
+                        ]}
+                      >
+                        {booth.render()}
+                      </Animated.View>
+                    );
+                  })}
+              </Animated.View>
+            </GestureDetector>  :       <Image
+              source={{ uri: src }}
+              style={{ display: 'flex', height: 200, width: 200, opacity: 0, position: 'absolute', zIndex: -1 }}
+              onLoad={({
+                nativeEvent: {
+                  source: { width, height },
+                },
+              }) => {
+                if (width && height) {
+                  setImageWidth(width);
+                  setImageHeight(height);
+                  
+                }
+              }}
+      
+              onLoadEnd={() => {
+                setIsLoaded(true);
+              }}
+/>
+    }
+
+    </View>
   );
 };
 
